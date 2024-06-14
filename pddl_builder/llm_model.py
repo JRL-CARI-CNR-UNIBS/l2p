1eab137ea37f257fd03bac888a7cb3a31dc66321
Marcus Tantakoun <mtantakoun@gmail.com>
2024-06-14 12:02:19 -0400

    Added functions for domain generation

--- pddl_builder/llm_model.py
+++ pddl_builder/llm_model.py
@@ -2,20 +2,38 @@
 This file contains code for calling LLMs and saving raw model outputs 
 """
 
+import os
+import time
+import json
+import copy
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-1.3B"
-headers = {"Authorization": "Bearer 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"}
+#model_name = "openai-community/gpt2"
+model_name = "meta-llama/Meta-Llama-3-8B"
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": "Generate a PDDL file"
-})
+api_key = "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"
 
-print(output)
+API_URL = f"https://api-inference.huggingface.co/models/{model_name}"
+
+headers = {"Authorization": f"Bearer {api_key}",
+           "Content-Type": "application/json"}
+
+def query(prompt, max_length=50, temperature=0.7):
+    data = {
+        "inputs": prompt,
+        "parameters": {
+            "max_length": max_length,
+            "temperature": temperature
+        }
+    }
+
+    response = requests.post(API_URL, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()[0]['generated_text']
+    else:
+        raise Exception(f"Failed to generate text: {response.status_code} {response.text}")
 
 if __name__ == "__main__":
-    pass
+    prompt = "This is my PDDL file:"
+    generated_text = query(prompt)
+    print(generated_text)
