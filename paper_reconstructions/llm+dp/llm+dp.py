9ad969fe6af96a190d0ebc6c701d9a290da899ab
Marcus Tantakoun <mtantakoun@gmail.com>
2024-11-19 22:03:07 -0500

    removed incomplete paper_reconstruction files

--- paper_reconstructions/llm+dp/llm+dp.py
+++
@@ -1,7 +0,0 @@
-"""
-Paper: "Dynamic Planning with a LLM" Dagan et al. (2023)
-Source code: https://github.com/itl-ed/llm-dp
-Run: python3 -m paper_reconstructions.llm+dp.llm+dp
-
-Will implement in the future ...
-"""
