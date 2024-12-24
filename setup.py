965551744444e5920b99e9845e84823bf15dc4c6
Christian Muise <christian.muise@queensu.ca>
2024-12-23 22:41:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lang2pddl",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(exclude=["tests"]),
     description="Library to connect LLMs and planning tasks",
     long_description=long_description,
